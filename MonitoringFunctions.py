224f10eb4ec1e66153fdf0160eba4516ad033b40
Oracle Public Cloud User <opc@automation.subnet05061101.vcn05061101.oraclevcn.com>
2023-06-25 10:32:47 +0200

    Add support for any schedule key

--- MonitoringFunctions.py
+++ MonitoringFunctions.py
@@ -3,8 +3,6 @@ import datetime
 import json
 import pandas as pd
 
-#TODO: Load schedule tag dinamically (now is "hard" set to 'AnyDay')
-
 def getInstanceStatusMetrics(config, signer, OCID, CompartmentId, ScheduleTags):
     # Select metric namespace and metric name for Instance
     metric_namespace = "oci_compute_infrastructure_health"
@@ -38,9 +36,12 @@ def getInstanceStatusMetrics(config, signer, OCID, CompartmentId, ScheduleTags):
         position = 0
         for hour in range(24):
             now_iso = now - datetime.timedelta(days=day, hours=hour)
+
+            # Add label (X-Axis)
             labels.append(now_iso.isoformat())
             
-            # TODO: Improve timestamp search
+            # Add resource status value (Y-Axis)
+            ## TODO: Improve timestamp search
             found = 0
             for item in metric_data:
                 if item.timestamp == now_iso:
@@ -54,7 +55,19 @@ def getInstanceStatusMetrics(config, signer, OCID, CompartmentId, ScheduleTags):
                 # For instance_status metric, when instance is stopped no value is found
                 status_data.append(0)
 
-            schedule_data.append(ScheduleTags['AnyDay'][position])
+            # Add resource schedule value (Y-Axis)
+            ## Schedule values are added by priority: Day of month > Day of week > WeekDay or WeekEnd > AnyDay
+            if now_iso.day in ScheduleTags and now_iso.day == ScheduleTags[now_iso.day].key():
+                schedule_data.append(ScheduleTags[now_iso.day][position])
+            elif now_iso.strftime('%A') in ScheduleTags and now_iso.strftime('%A') == ScheduleTags[now_iso.strftime('%A')].key():
+                schedule_data.append(ScheduleTags[now_iso.strftime('%A')][position])
+            elif 'WeekDay' in ScheduleTags and now_iso.weeday() < 5:
+                schedule_data.append(ScheduleTags['WeekDay'][position])
+            elif 'WeekEnd' in ScheduleTags and now_iso.weekday() > 4:
+                schedule_data.append(ScheduleTags['WeekEnd'][position])
+            else:
+                 schedule_data.append(ScheduleTags['AnyDay'][position])
+
             position += 2
 
     monitoring_data = {"labels": labels, "status_data": status_data, "schedule_data": schedule_data}
@@ -94,9 +107,12 @@ def getDbSystemStatusMetrics(config, signer, OCID, CompartmentId, ScheduleTags):
         position = 0
         for hour in range(24):
             now_iso = now - datetime.timedelta(days=day, hours=hour)
+
+            # Add label (X-Axis)
             labels.append(now_iso.isoformat())
             
-            # TODO: Improve timestamp search
+            # Add resource status value (Y-Axis)
+            ## TODO: Improve timestamp search
             found = 0
             for item in metric_data:
                 if item.timestamp == now_iso:
@@ -108,7 +124,19 @@ def getDbSystemStatusMetrics(config, signer, OCID, CompartmentId, ScheduleTags):
                 # For instance_status metric, when instance is stopped no value is found
                 status_data.append(0)
 
-            schedule_data.append(ScheduleTags['AnyDay'][position])
+            # Add resource schedule value (Y-Axis)
+            ## Schedule values are added by priority: Day of month > Day of week > WeekDay or WeekEnd > AnyDay
+            if now_iso.day in ScheduleTags and now_iso.day == ScheduleTags[now_iso.day].key():
+                schedule_data.append(ScheduleTags[now_iso.day][position])
+            elif now_iso.strftime('%A') in ScheduleTags and now_iso.strftime('%A') == ScheduleTags[now_iso.strftime('%A')].key():
+                schedule_data.append(ScheduleTags[now_iso.strftime('%A')][position])
+            elif 'WeekDay' in ScheduleTags and now_iso.weeday() < 5:
+                schedule_data.append(ScheduleTags['WeekDay'][position])
+            elif 'WeekEnd' in ScheduleTags and now_iso.weekday() > 4:
+                schedule_data.append(ScheduleTags['WeekEnd'][position])
+            else:
+                 schedule_data.append(ScheduleTags['AnyDay'][position])
+                 
             position += 2
 
     monitoring_data = {"labels": labels, "status_data": status_data, "schedule_data": schedule_data}
